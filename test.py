8f2abb0bb6a4d4fceeaa0b11e02b3964e1d6cbb6
Ar-Vi <aryavishnu06@gmail.com>
2025-02-02 11:37:29 -0500

    Final Push

--- test.py
+++ test.py
@@ -7,19 +7,30 @@ BAUD_RATE = 9600
 
 measurement = eyeTracker()
 print(measurement,"FROM TEST")
+# measurement = 30.62
 
 try:
     arduino = serial.Serial(ARDUINO_PORT, BAUD_RATE, timeout=1)
     time.sleep(2)  # Allow time for connection
 
     while True:
-        command = input("Enter 'START' to run motor, 'STOP' to stop motor: ").strip().upper()
+        command = input("Enter 'START' to start process: ").strip().upper()
         
-        if command in ["START", "STOP"]:
+        if command in ["START"]:
+            arduino.write(f"{str(measurement)}\n".encode())  # Send command to Arduino
+            print(f"Sent: {command}")
+            break
+        else:
+            print("Invalid command. Type 'START'")
+    while True:
+        command = input("Enter 'LEFT' or 'RIGHT' to adjust: ").strip().upper()
+        
+        if command in ["LEFT","RIGHT"]:
             arduino.write(f"{command}\n".encode())  # Send command to Arduino
             print(f"Sent: {command}")
         else:
-            print("Invalid command. Type 'START' or 'STOP'.")
+            print("Invalid command.")
+
 
 except serial.SerialException as e:
     print(f"Error connecting to Arduino: {e}")
